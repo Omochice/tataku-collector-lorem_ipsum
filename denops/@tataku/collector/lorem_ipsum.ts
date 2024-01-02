5b586cf8b9b16766cf602f4c36108740a39dbcdf
Omochice <mochice.mls.ntl@gmail.com>
2024-01-02 19:11:41 +0900

    feat!: include new line

--- denops/@tataku/collector/lorem_ipsum.ts
+++ denops/@tataku/collector/lorem_ipsum.ts
@@ -1,10 +1,10 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
 
 const loremIpsum = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n",
+  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n",
+  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n",
+  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n",
 ];
 
 const collector = (_denops: Denops, _option: unknown) => {
